159cb4576d9d69d547607c49cb9ad896fa81d3b1
vanderlei35sobrinho <vanderlei36035@gmail.com>
2022-09-20 17:13:10 -0300

    read

--- recipes/views.py
+++ recipes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name': 'vanderson moura',
     })
     
